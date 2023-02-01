ec897aed8fa7d824ef628e35850b24117a665aa6
Ericwyn <ericwyn.chen@gmail.com>
2023-02-01 20:16:50 +0800

    [ui][terminal] update terminal completion script

--- eze-share-completion.bash
+++ eze-share-completion.bash
@@ -1,21 +1,37 @@
 #/usr/bin/env bash
 function _myscript(){
-    if [[ "${COMP_CWORD}" == "1" ]];then
+    if [[ "${COMP_CWORD}" == "1" ]];
+    then
         COMP_WORD="-receiver -sender"
         COMPREPLY=($(compgen -W "$COMP_WORD" -- ${COMP_WORDS[${COMP_CWORD}]}))
     else
-        case ${COMP_WORDS[$[$COMP_CWORD-1]]} in
-        -receiver)
-        COMP_WORD_2="-ip -debug"
-        COMPREPLY=($(compgen -W "${COMP_WORD_2}" ${COMP_WORDS[${COMP_CWORD}]}))
-        ;;
+        if [[ "${COMP_CWORD}" == "2" ]];
+        then
+            case ${COMP_WORDS[$[$COMP_CWORD-1]]} in
+            -receiver)
+            COMP_WORD_2="-ip -debug"
+            echo ''
+            echo '2222-receiver'
+            echo ''
+            COMPREPLY=($(compgen -W "${COMP_WORD_2}" ${COMP_WORDS[${COMP_CWORD}]}))
+            ;;
 
-        -sender)
-        COMP_WORD_2="-ip -f -debug"
-        COMPREPLY=($(compgen -W "${COMP_WORD_2}" ${COMP_WORDS[${COMP_CWORD}]}))
-        ;;
+            -sender)
+            COMP_WORD_2="-ip -f -debug"
+            COMPREPLY=($(compgen -W "${COMP_WORD_2}" ${COMP_WORDS[${COMP_CWORD}]}))
+            ;;
 
-        esac
+            esac
+
+        elif [[ "${COMP_CWORD}" == "3" ]];
+        then
+            case ${COMP_WORDS[$[$COMP_CWORD-1]]} in
+            -f)
+            COMPREPLY=($(compgen -f ${COMP_WORDS[${COMP_CWORD}]}))
+            ;;
+
+            esac
+        fi
     fi
 }
 # 注册命令补全函数
